b8fa565a15fc567d83ae4a11bd3d05eeb1426dd6
qinshuqian <524477767@qq.com>
2019-03-16 23:34:43 +0800

    update

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -5,10 +5,10 @@ import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
-    Map<String,Integer> countSameElements(List<String> collection1) {
+    Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-        Map<String,Integer> map = new HashMap<>();
-        collection1.forEach(i -> map.put(i,map.containsKey(i) ? map.get(i)+1 : 1));
-        return map;
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        collection1.forEach(s -> hashMap.put(s, hashMap.containsKey(s) ? hashMap.get(s) + 1 : 1));
+        return hashMap;
     }
 }
--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,31 +1,32 @@
 package cn.school.thoughtworks.section2;
 
-import javafx.scene.shape.VLineTo;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
-        Map<String,Integer> map = new HashMap<>();
-        collection1.forEach(i -> {
-            String key = "";
-            int value = 1;
-            if(i.contains("[")){
-                int index = i.indexOf("[");
-                key = i.substring(0,index);
-                value = Integer.parseInt(i.substring(index+1,i.length()-1));
-            }else if(i.contains("-") || i.contains(":")){
-                String[] s = i.split("[:-]");
-                key = s[0];
-                value = Integer.parseInt(s[1]);
-            }else {
-                key = i;
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        collection1.forEach(a -> {
+            StringBuilder key = new StringBuilder();
+            String regex = "^(\\w+)\\[(\\d+)]$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(a);
+            int number = 1;
+            if (a.contains("-") || a.contains(":")) {
+                String[] split = a.split("[-:]");
+                key.append(split[0]);
+                number = Integer.parseInt(split[1]);
+            } else if (matcher.find()) {
+                key.append(matcher.group(1));
+                number = Integer.parseInt(matcher.group(2));
+            } else {
+                key.append(a);
             }
-            map.put(key,map.containsKey(key) ? map.get(key)+value : value);
+            hashMap.put(key.toString(), hashMap.containsKey(key.toString()) ? hashMap.get(key.toString()) + number : number);
         });
-        return map;
+        return hashMap;
     }
 }
